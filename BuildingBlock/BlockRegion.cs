8c61918e0ddf10a2808e135d3dfbd8ceb8d88294
James Company <JamesCo>
2025-06-01 02:39:47 +0100

    Add project files.

---
+++ BuildingBlock/BlockRegion.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace BuildingBlock
+{
+    public class JsonConfig
+    {
+        public List<ushort> AffectedBarricades { get; set; } = new List<ushort>();
+        public List<BlockRegion> BlockRegions { get; set; } = new List<BlockRegion>();
+
+        public void Write(string levelName, string directory)
+        {
+            string filePath = Path.Combine(directory, $"{levelName}.json");
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+        public static JsonConfig Load(string levelName, string directory)
+        {
+            string filePath = Path.Combine(directory, $"{levelName}.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<JsonConfig>(json);
+            }
+            return null;
+        }
+    }
+    public class BlockRegion
+    // way more complex than it should be but newtonsoft doesnt support vector3
+    {
+        [JsonIgnore]
+        public Vector3 Position { get; set; }
+        public float Distance { get; set; }
+
+        [JsonProperty("x")]
+        public float X
+        {
+            get => Position.x;
+            set => Position=new(value, Position.y, Position.z);
+        }
+        [JsonProperty("y")]
+        public float Y
+        {
+            get => Position.y;
+            set => Position=new(Position.x, value, Position.z);
+        }
+        [JsonProperty("z")]
+        public float Z
+        {
+            get => Position.z;
+            set => Position=new(Position.x, Position.y, value);
+        }
+
+        public BlockRegion(Vector3 pos, float distance)
+        {
+            Position=pos;
+            Distance=distance;
+        }
+        [JsonConstructor]
+        public BlockRegion(float x, float y, float z, float distance)
+        {
+            Position=new Vector3(x, y, z);
+            Distance=distance;
+        }
+    }
+}
---
+++ BuildingBlock/Main.cs
@@ -0,0 +1,112 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.Core.Commands;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using UnityEngine;
+
+namespace BuildingBlock
+{
+    public class Main : RocketPlugin
+    {
+        private JsonConfig loadeddConfig;
+        protected override void Load()
+        {
+            BarricadeManager.onDeployBarricadeRequested+=BarricadeReq;
+
+            Level.onLevelLoaded+=(_) =>
+            {
+                string mapName = Level.info?.name;
+                if (string.IsNullOrEmpty(mapName))
+                {
+                    Rocket.Core.Logging.Logger.Log("Failed to get map name???");
+                    return;
+                }
+                //Console.WriteLine(mapName);
+                JsonConfig config = JsonConfig.Load(mapName, Directory);
+                if (config==null)
+                {
+                    loadeddConfig=new JsonConfig();
+                    loadeddConfig.AffectedBarricades.Add(366);
+                    loadeddConfig.BlockRegions.Add(new(Vector3.zero, 10));
+                    loadeddConfig.Write(mapName, Directory);
+                }
+                else
+                {
+                    loadeddConfig=config;
+                }
+            };
+        }
+        protected override void Unload()
+        {
+            BarricadeManager.onDeployBarricadeRequested-=BarricadeReq;
+        }
+
+        public override TranslationList DefaultTranslations => new()
+        {
+            {"CANTBUILDHERE", "Cant build here" }
+        };
+
+        private void BarricadeReq(Barricade barricade,
+            ItemBarricadeAsset asset,
+            Transform hit,
+            ref Vector3 point,
+            ref float angle_x,
+            ref float angle_y,
+            ref float angle_z,
+            ref ulong owner,
+            ref ulong group,
+            ref bool shouldAllow)
+        {
+            ushort id = asset.id;
+            if (!loadeddConfig.AffectedBarricades.Contains(id))
+            {
+                return;
+            }
+
+            foreach (BlockRegion region in loadeddConfig.BlockRegions)
+            {
+                if (Vector3.Distance(region.Position, point)<=region.Distance)
+                {
+                    shouldAllow=false;
+                    UnturnedChat.Say((CSteamID)owner, Translate("CANTBUILDHERE"), color: Color.red);
+                    return;
+                }
+            }
+        }
+
+
+
+        [RocketCommand("setblockzone", "", AllowedCaller: AllowedCaller.Player)]
+        public void execute(IRocketPlayer caller, string[] args)
+        {
+            if (caller is not UnturnedPlayer player)
+            {
+                return;
+            }
+            if (args.Length<1)
+            {
+                UnturnedChat.Say(caller, "Use /setblockzone (distance)");
+                return;
+            }
+            if (!float.TryParse(args[0], out float distance))
+            {
+                UnturnedChat.Say(caller, "Invalid distance");
+                return;
+            }
+
+            Vector3 pos = player.Position;
+            BlockRegion region = new(pos, distance);
+            loadeddConfig.BlockRegions.Add(region);
+            loadeddConfig.Write(Level.info.name, Directory);
+
+            UnturnedChat.Say(caller, $"added with distance {distance}");
+        }
+
+    }
+}
